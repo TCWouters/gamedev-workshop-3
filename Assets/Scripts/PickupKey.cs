320942b66e728b0a6ef8a3793beb88b0c8128f0a
TCWouters <TC.Wouters1@student.han.nl>
2026-03-10 15:01:07 +0100

    keys

--- Assets/Scripts/PickupKey.cs
+++ Assets/Scripts/PickupKey.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PickupKey : MonoBehaviour
 {
     public static int keyCount = 0;
+    public Collider doorCollider;
 
     void OnTriggerEnter(Collider other)
     {
@@ -11,6 +12,14 @@ public class PickupKey : MonoBehaviour
         {
             keyCount++;
             Debug.Log("Key picked up! Total keys: " + keyCount);
+            
+            // If 3 keys are collected, disable the door collider
+            if (keyCount >= 3 && doorCollider != null)
+            {
+                doorCollider.enabled = false;
+                Debug.Log("Door unlocked!");
+            }
+            
             Destroy(gameObject);
         }
     }
